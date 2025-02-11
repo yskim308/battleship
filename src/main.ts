3fb5a3351046c18823928536660124d2dee7ac12
Young Seo Kim <youngseokim7@gmail.com>
2025-02-11 19:18:22 +0900

    function prototypes for main

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-import './styles.css';
+import "./styles.css";
+import { Player, PlayerStatus } from "./playerClass";
+import { updateGrid } from "./domManipulator";
+
+function randomShips() {
+  // place random ships
+}
+
+function waitClick() {
+  // return promise for waiting for a click
+}
+
+async function playGame() {
+  //main play function should return when game over
+}
